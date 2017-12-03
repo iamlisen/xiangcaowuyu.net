bce82eb598f23702f0c31ff4257699c97f50c7e9
iamlisen@163.com <l19900629>
2017-12-03 22:53:14 +0800

    * Article.cs: 333
    * Index.cshtml:

--- xiangcaowuyu.net/Models/Entity/Article.cs
+++ xiangcaowuyu.net/Models/Entity/Article.cs
@@ -11,12 +11,17 @@ namespace xiangcaowuyu.net.Models.Entity
         [Key]
         public string ID { get; set; }
         [Required(ErrorMessage = "请输入文章标题")]
+        [Display(Name = "标题")]
         [MaxLength(200, ErrorMessage = "文章标题不能超过50个字符")]
         public string Title { get; set; }
         [Required(ErrorMessage = "请输入文章内容")]
+        [Display(Name = "内容")]
         public string Context { get; set; }
+        [Display(Name = "发布日期")]
         public DateTime? PostTime { get; set; }
+        [Display(Name = "是否推荐")]
         public int IsHot { get; set; }
+        [Display(Name = "类型")]
         public string Type { get; set; }
     }
 }
